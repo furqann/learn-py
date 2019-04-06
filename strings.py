9f7b8af9b045b5d8d0446a01c7719bd1ee3e3ec7
Furqan <furqanahmad21@hotmail.com>
2019-04-06 18:30:58 +0500

    5. String in python

---
+++ strings.py
@@ -0,0 +1,7 @@
+lang = "python for experts"
+lang_arr = lang[:]  # This will copy the whole string.
+print(lang[:3])  # Left default value is 0
+print(lang[0:3])  # Output will be pyt
+print(lang[1:3])  # Output will be yt
+#  What will be the output of lang[1:-1] ?
+#  print(lang[1:-1])
