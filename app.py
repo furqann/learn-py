149801e89fecbe80f0c2c0727868b8d04058f0c0
Furqan <furqanahmad21@hotmail.com>
2019-04-06 14:34:59 +0500

    Initial commit & print statement

---
+++ app.py
@@ -0,0 +1,10 @@
+# Python interpreter excutes program line by line.
+
+# Simple print statements
+print("Furqan")
+print('\/')
+print('O ---- /')
+print(' /\ /\\')
+
+# print statement as a expression.
+print('*' * 10)
