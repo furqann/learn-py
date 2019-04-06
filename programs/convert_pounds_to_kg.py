eff20e625c38aa66ef7579de8f0d029f56150fd7
Furqan <furqanahmad21@hotmail.com>
2019-04-06 15:48:30 +0500

    Made some changes to convert_pounds_to_kg variable naming

--- programs/convert_pounds_to_kg.py
+++ programs/convert_pounds_to_kg.py
@@ -1,7 +1,7 @@
 # Common builtin functions
 # int(), float(), str(), bool()
-pounds = input("Your weight in pounds: ")  # Default is string. If you expecting any number, you must change it type.
-print(type(pounds))
-kilograms = float(pounds) / 2.205
-print(type(kilograms))
-print(kilograms)
+weight_lbs = input("Your weight in pounds: ")  # Default is string. If you expecting any number, you must change it type.
+print(type(weight_lbs))  # Shows the type of the variable
+weight_kg = float(weight_lbs) * 0.45
+print(type(weight_kg))  # Shows the type of the variable
+print(weight_kg)
