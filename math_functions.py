018ca90de45a74ba7a625a9a96d655fe61d6718a
Furqan <furqanahmad21@hotmail.com>
2019-04-07 14:00:17 +0500

    7. Math functions and math module

---
+++ math_functions.py
@@ -0,0 +1,4 @@
+import math  # This math module in python for complex mathematical functionality
+#  For more detail visit -> https://docs.python.org/3/library/math.html
+print(math.ceil(2.9))
+print(math.floor(2.9))
