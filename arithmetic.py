625857a1d4d0569d97c8cf69f73a595db2183f77
Furqan <furqanahmad21@hotmail.com>
2019-04-07 13:48:57 +0500

    6. Arithmetic operations

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+#  Arithmetic Operations in python
+print(10 + 3)  # Simple addition Integers
+print(10 + 3.32)  # Floating addition
+print(10 - 3)  # Integer subtraction
+print(10 / 3)  # Will return float number
+print(10 * 3)  # Will return float number
+print(10 // 3)  # Will return integer number
+print(10 % 3)  # Will return the remainder
+print(10 ** 3)  # Exponent operator will return
+
+#  Operators precedence
+#  () -> ** -> * or / -> + or -
