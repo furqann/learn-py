7a464df061c4d1dd0364df89a312a43ffeec07c1
Furqan <furqanahmad21@hotmail.com>
2019-04-07 15:18:49 +0500

    8. Decision statements

---
+++ decision_statement.py
@@ -0,0 +1,20 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("Coffee is hot")
+elif is_cold:
+    print("Coffee is cold")
+    print("I love cold coffee")
+else:
+    print("Chocolate coffee yum! nor hot nor cold")
+print("Enjoy your coffee")
+
+#  Exercise
+house_price = 10**6
+has_good_credit = True
+if has_good_credit:
+    down_payment = house_price * 0.1
+else:
+    down_payment = house_price * 0.2
+print(f"Down payment required: ${down_payment}")
