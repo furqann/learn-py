c85246a9b51d9594999fb4b5062ecbb555aca2d1
Furqan <furqanahmad21@hotmail.com>
2019-04-09 23:38:06 +0500

    For loop exercise

--- loop.py
+++ loop.py
@@ -13,3 +13,11 @@ for item in range(10,15):
     print(item)
 for item in range(20,30,2):
     print(item)
+
+#  Exercise
+#  Print the total cost of items in a list
+itemList = [10, 20, 30]
+totalCost = 0
+for item in itemList:
+    totalCost += item
+print(f"Total cost: {totalCost}")
