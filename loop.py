79f777a7215dfe9c5f56b5c7c6f7ae055dc82b35
Furqan <furqanahmad21@hotmail.com>
2019-04-09 23:27:36 +0500

    11. For loop with range function

---
+++ loop.py
@@ -0,0 +1,11 @@
+#  Basic for loop
+for item in 'Python':
+    print(item)
+for item in ['John','Ali','Sana']:
+    print(item)
+for item in [0, 20]:
+    print(item)
+
+#  Range function in loop
+for item in range(5):
+    print(item)
