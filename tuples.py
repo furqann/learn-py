fdca6758ed3a9e93a1c439ba6a40db7cfaef8853
Furqan <furqanahmad21@hotmail.com>
2019-04-11 23:53:42 +0500

    14. Unpacking

--- tuples.py
+++ tuples.py
@@ -1,3 +1,13 @@
 #  Tuples are like list but these are immutable
-numbers = (1, 2, 3)
+numbers = (1, 2, 3)  # For tuples we use parentheses
 print(numbers)
+
+#  Unpacking (You can do the same with list as well)
+coordinates = (14, 2, 3)
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+# Above all lines can be written as below.
+x, y, z = coordinates
+print(x)
+print(y)
