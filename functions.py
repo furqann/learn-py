8b3642cbfbcffb3508549d45de1117ad68c01fe7
Furqan <furqanahmad21@hotmail.com>
2019-04-12 23:46:09 +0500

    16. Functions

---
+++ functions.py
@@ -0,0 +1,8 @@
+#  Different types of functions
+def greet_user(first_name, last_name):
+    print(f"Hi, {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greet_user("John", "Shawn")  # Positional arguments
+greet_user(last_name="John", first_name="Shawn")  # Keyword arguments
