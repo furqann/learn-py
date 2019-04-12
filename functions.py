2f162d874783680871052ae9671dc73867985ed7
Furqan <furqanahmad21@hotmail.com>
2019-04-12 23:56:10 +0500

    Functions with return keyword

--- functions.py
+++ functions.py
@@ -6,3 +6,12 @@ def greet_user(first_name, last_name):
 
 greet_user("John", "Shawn")  # Positional arguments
 greet_user(last_name="John", first_name="Shawn")  # Keyword arguments
+
+#  Return function
+#  All functions in python returns the value None
+
+def square(number):
+    return number*number
+
+
+print(square(2))
