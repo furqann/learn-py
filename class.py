bce28d4e7e9fa3fa1b3cc696bb6689cada95a410
Furqan <furqanahmad21@hotmail.com>
2019-04-13 10:47:37 +0500

    Class Constructor

--- class.py
+++ class.py
@@ -1,13 +1,16 @@
 #  Classes
 class Person:
+    def __init__(self, name):  # Constructor
+        self.name = name
+
     def print_name(self):
-        print("Some name")
+        print(self.name)
 
     def print_address(self):
         print("Some address")
 
 
-person1 = Person()  # Initialize
-person1.name = "Reese"  # Property
-person1.print_address()
+person1 = Person("John")  # Initialize
+# person1.name = "Reese"  # Property
+person1.print_name()
 print(person1.name)
