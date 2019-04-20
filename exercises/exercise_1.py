c80650373f7fa227a7ad17b133c3fb3a49826e64
Furqan <furqanahmad21@hotmail.com>
2019-04-21 02:08:44 +0500

    Refactored: exercise_1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,9 +1,9 @@
 #  Exercise from https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
-import datetime
-
+from datetime import datetime as dt
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+current_age = int(input("Enter your age: "))
 total_copies = int(input("How many times you want to print: "))
-year = datetime.datetime.now()
-hundred_year = (100 - age) + year.year
-print(f"You'll be 100 year old in {hundred_year}\n" * total_copies)
+present_year = dt.now().year
+age_to_be = 100
+hundred_year_in = (age_to_be - current_age) + present_year
+print(f"You'll be 100 year old in {hundred_year_in}\n" * total_copies)
