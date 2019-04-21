555a22f2bae8ce8ef70a003ce6fb163a1436d5e5
Furqan <furqanahmad21@hotmail.com>
2019-04-21 13:48:27 +0500

    Extras added exercise 3

--- exercises/list_less_than_ten_3.py
+++ exercises/list_less_than_ten_3.py
@@ -1,4 +1,9 @@
 numbers_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+user_input = int(input("Enter your number to get list of numbers less than: "))
+new_list = []
 for num in numbers_list:
-    if num < 5:
-        print(f"{num} is less than 5")
+    if num < user_input:
+        new_list.append(num)
+
+for num in new_list:
+    print(num)
