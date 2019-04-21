1e545b74e3d88c3905185943187fc194e09a15d2
Furqan <furqanahmad21@hotmail.com>
2019-04-21 14:15:45 +0500

    Exercise 3 word corrention

--- exercises/list_less_than_ten_3.py
+++ exercises/list_less_than_ten_3.py
@@ -1,6 +1,6 @@
 numbers_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 user_input = int(input("Enter your number to get list of numbers less than: "))
-#  This method is know as list comprehension.
+#  This method is known as list comprehension.
 new_list = [num for num in numbers_list if num < user_input]
 print([item for item in new_list])  # Printing in one line
