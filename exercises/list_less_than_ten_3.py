18d38e5588dc032d976b15db0ce5e4fa087f71ca
Furqan <furqanahmad21@hotmail.com>
2019-04-21 14:12:49 +0500

    Solved the exercise 3 using list comprehension

--- exercises/list_less_than_ten_3.py
+++ exercises/list_less_than_ten_3.py
@@ -1,9 +1,6 @@
 numbers_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-user_input = int(input("Enter your number to get list of numbers less than: "))
-new_list = []
-for num in numbers_list:
-    if num < user_input:
-        new_list.append(num)
 
-for num in new_list:
-    print(num)
+user_input = int(input("Enter your number to get list of numbers less than: "))
+#  This method is know as list comprehension.
+new_list = [num for num in numbers_list if num < user_input]
+print([item for item in new_list])  # Printing in one line
