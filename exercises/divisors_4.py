abd873e6a25ffb53a5ec53c9d16a355f77f78ee4
Furqan <furqanahmad21@hotmail.com>
2019-04-22 23:33:51 +0500

    Refactored find all divisor of a given number exercise 4

--- exercises/divisors_4.py
+++ exercises/divisors_4.py
@@ -1,6 +1,8 @@
-find_divisors = int(input("Find divisor: "))
+find_divisors = int(input("Find divisors: "))
+
 divisor_list = []
-for item in range(1, 101):
+for item in range(1, find_divisors + 1):
     if find_divisors % item == 0:
         divisor_list.append(item)
+
 print(divisor_list)
