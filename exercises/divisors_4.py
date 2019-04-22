74c5d7174226e690d180089f5827cb94e702d3a7
Furqan <furqanahmad21@hotmail.com>
2019-04-22 23:38:55 +0500

    For loop in one line E4

--- exercises/divisors_4.py
+++ exercises/divisors_4.py
@@ -1,8 +1,4 @@
 find_divisors = int(input("Find divisors: "))
-
-divisor_list = []
-for item in range(1, find_divisors + 1):
-    if find_divisors % item == 0:
-        divisor_list.append(item)
-
+check_upto = find_divisors + 1
+divisor_list = [divisor for divisor in range(1, check_upto) if find_divisors % divisor == 0]
 print(divisor_list)
