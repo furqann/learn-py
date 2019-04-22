dd2deda8313d63e502d253cd48a69dacd3006e64
Furqan <furqanahmad21@hotmail.com>
2019-04-22 23:40:42 +0500

    Add question link E4

--- exercises/divisors_4.py
+++ exercises/divisors_4.py
@@ -1,3 +1,4 @@
+#  https://www.practicepython.org/exercise/2014/02/26/04-divisors.html
 find_divisors = int(input("Find divisors: "))
 check_upto = find_divisors + 1
 divisor_list = [divisor for divisor in range(1, check_upto) if find_divisors % divisor == 0]
