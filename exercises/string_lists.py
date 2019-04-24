41d0818b99c42d5a418da3b15aac7d26662d924d
Furqan <furqanahmad21@hotmail.com>
2019-04-24 23:21:40 +0500

    WIP: Exercise 5

---
+++ exercises/string_lists.py
@@ -0,0 +1,6 @@
+#  https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
+#  Whether the input string is palindrome
+user_input = input("Enter a palindrome: ")
+print(-user_input.__len__())
+reverse = user_input[-1:0]
+print(reverse)
