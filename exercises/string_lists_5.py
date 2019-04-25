4625b20ac186e6c3df49b9c75cf1d028b081bf12
Furqan <furqanahmad21@hotmail.com>
2019-04-25 23:29:04 +0500

    Exercise 5 completed

--- exercises/string_lists_5.py
+++ exercises/string_lists_5.py
@@ -1,6 +1,12 @@
 #  https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 #  Whether the input string is palindrome
+
+
+def is_palindrome(word):
+    to_string = str(word)
+    reverse = to_string[::-1]
+    return reverse == to_string
+
+
 user_input = input("Enter a palindrome: ")
-print(-user_input.__len__())
-reverse = user_input[-1:0]
-print(reverse)
+print(is_palindrome(user_input))
