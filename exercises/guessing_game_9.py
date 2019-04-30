2cd7d342cfdae127899d92369efa2c7e7b1785f5
Furqan <furqanahmad21@hotmail.com>
2019-05-01 01:54:58 +0500

    Refactored solution of exercise 9

--- exercises/guessing_game_9.py
+++ exercises/guessing_game_9.py
@@ -1,13 +1,14 @@
 #  https://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
-import random as r
-import math as m
+from random import randint as random_integer
 
-random_num = m.ceil(r.random()*10)
+MIN = 1
+MAX = 10
+random_num = random_integer(MIN, MAX)
 total_try = 0
 
 while True:
     user_guess = input("Your Guess: ")
-    if user_guess == "exit":
+    if user_guess.lower() == "exit":
         break
     total_try += 1
     user_guess = int(user_guess)
@@ -17,5 +18,3 @@ while True:
         print("too low")
     else:
         print(f"Correct {user_guess} is equal to {random_num} in {total_try} tries")
-        break
-
