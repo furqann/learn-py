60a008fdab8966b94c34b96aa801489c40849970
Furqan <furqanahmad21@hotmail.com>
2019-05-06 23:58:34 +0500

    E11 Working example but not good, took forever for fairly long number

--- exercises/check_primality_11.py
+++ exercises/check_primality_11.py
@@ -2,32 +2,16 @@
 """
 Ask the user for a number and determine whether the number is prime or not.
 """
-
-
-def is_prime(number):
-    if number % 2 == 0:
-        print("Prime")
-    elif number % 3 == 0:
-        print("Prime")
-    elif number % 4 == 0:
-        print("Prime")
-    elif number % 5 == 0:
-        print("Prime")
-    elif number % 6 == 0:
-        print("Prime")
-    elif number % 8 == 0:
-        print("Prime")
-    elif number % 9 == 0:
-        print("Prime")
-    elif number % 10 == 0:
-        print("Prime")
-    else:
-        print("Not Prime")
-
-
 user_input = int(input("Enter number to check it is prime: "))
-if user_input == 1:
+
+if user_input <= 0:
+    print("Invalid input")
+elif user_input == 1:
     print("Not Prime")
 else:
-    is_prime(user_input)
+    divided_by = [num for num in range(2, user_input+1) if user_input % num == 0]
+    if len(divided_by) > 1:
+        print("Not prime")
+    else:
+        print("Prime")
 
