854389a97f0fbd961b0dd29d03de9f069996571f
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-05-07 23:35:00 +0500

    Create list_ends_12.py
    WIP: Added exercise 12 list_ends

---
+++ exercises/list_ends_12.py
@@ -0,0 +1,6 @@
+#  https://www.practicepython.org/exercise/2014/04/25/12-list-ends.html
+"""
+Write a program that takes a list of numbers (e.g, a = [5, 10, 15, 20, 25]) and 
+makes a new list of only the first and last elements of the given list. 
+For practice, write this code inside a function.
+"""
