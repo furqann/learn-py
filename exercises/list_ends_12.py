290bafe49e0332356cc676846fa11baf494a28c3
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-05-08 22:59:35 +0500

    Update list_ends_12.py
    Exercise 12 Completed

--- exercises/list_ends_12.py
+++ exercises/list_ends_12.py
@@ -4,3 +4,14 @@ Write a program that takes a list of numbers (e.g, a = [5, 10, 15, 20, 25]) and
 makes a new list of only the first and last elements of the given list. 
 For practice, write this code inside a function.
 """
+def get_first_last_item(list_num):
+  if len(list_num) == 0:
+    return []
+  elif len(list_num) == 1:
+    return list_num
+  elif len(list_num) >= 1:
+    return [list_num[0],list_num[-1]]
+
+  print(get_first_last_item([1,2,34,5,7]))
+  print(get_first_last_item([]))
+  print(get_first_last_item([2,3]))
