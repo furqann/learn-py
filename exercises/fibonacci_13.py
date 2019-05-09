9d3b3edde174902ade2ea1f72d4f7092287123cb
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-05-09 23:44:14 +0500

    Exercise 13 completed

--- exercises/fibonacci_13.py
+++ exercises/fibonacci_13.py
@@ -1,1 +1,11 @@
 #  https://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
+def fib(num):
+  if num == 1:
+    return 1
+  elif num == 0:
+    return 0
+  return fib(num - 1) + fib(num - 2)
+
+
+print(fib(1))
+print(fib(10))
