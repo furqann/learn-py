bac897dfc8ed32a6e94b0a1406eb2865fda54b12
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-05-16 23:14:33 +0500

    Update password_gen_16.py

--- exercises/password_gen_16.py
+++ exercises/password_gen_16.py
@@ -1,4 +1,5 @@
 #  https://www.practicepython.org/exercise/2014/05/28/16-password-generator.html
+#  http://www.asciitable.com/
 """
 Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols.
 The passwords should be random, generating a new password every time the user asks for a new password.
@@ -7,3 +8,21 @@ Include your run-time code in a main method.
   => Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
 """
 import random as r
+
+
+def ran_gen():
+  lower_alpha = chr(r.randint(97, 122))
+  upper_alpha = chr(r.randint(69,78))
+  special_char = chr(r.randint(33,47))
+  numbers = str(r.randint(0,999))
+  return lower_alpha + upper_alpha + special_char + numbers
+
+
+password = ""
+iterations = 5
+
+while iterations > 0:
+  password += ran_gen() 
+  iterations -= 1
+
+print(password)
